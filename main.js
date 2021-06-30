d5b8f29a5d801615a49f57dfa66e7fada6c9e8fc
Marcelo Frederico Slabcouski Branco Garcia <prof.marcelo.garcia@gmail.com>
2021-06-30 17:12:40 -0300

    Quarta Aula

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (const link of links) {
   })
 }
 /*mudar o header da página quando der o scroll*/
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
-window.addEventListener('scroll', function () {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -25,7 +25,7 @@ window.addEventListener('scroll', function () {
     //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
 /* Testimonials carousel slider swiper */
 const swiper = new Swiper('.swiper-container', {
   slidesPerView: 1,
@@ -34,4 +34,34 @@ const swiper = new Swiper('.swiper-container', {
   },
   mousewheel: true,
   keyboard: true
+})
+/* ScrollReaveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .text, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links,
+  footer .brand, footer .social
+  `,
+  { interval: 100 }
+)
+/* Botão voltar para o top */
+function backToTop() {
+  const backToTopButton = document.querySelector('.back-to-top')
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll(), backToTop()
 })
