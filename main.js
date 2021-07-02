ad8532e363bdf7b03964d66d3f6b627349f708e7
Marcelo Frederico Slabcouski Branco Garcia <prof.marcelo.garcia@gmail.com>
2021-07-02 13:55:03 -0300

    Aula Final

--- main.js
+++ main.js
@@ -15,9 +15,9 @@ for (const link of links) {
   })
 }
 /*mudar o header da página quando der o scroll*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 function changeHeaderWhenScroll() {
-  const header = document.querySelector('#header')
-  const navHeight = header.offsetHeight
   if (window.scrollY >= navHeight) {
     //scroll é maior que a altura do header
     header.classList.add('scroll')
@@ -33,7 +33,13 @@ const swiper = new Swiper('.swiper-container', {
     el: '.swiper-pagination'
   },
   mousewheel: true,
-  keyboard: true
+  keyboard: true,
+  breakpoints: {
+    767: {
+      slidesPerView: 2,
+      setWrapperSize: true
+    }
+  }
 })
 /* ScrollReaveal: Mostrar elementos quando der scroll na página */
 const scrollReveal = ScrollReveal({
@@ -61,7 +67,30 @@ function backToTop() {
     backToTopButton.classList.remove('show')
   }
 }
+/* Menu ativo conforme a seção visível na página */
+const sections = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection() {
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+  for (const section of sections) {
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+    if (checkpointStart && checkpointEnd) {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.add('active')
+    } else {
+      document
+        .querySelector('nav ul li a[href*=' + sectionId + ']')
+        .classList.remove('active')
+    }
+  }
+}
 /* When Scroll */
 window.addEventListener('scroll', function () {
-  changeHeaderWhenScroll(), backToTop()
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
 })
